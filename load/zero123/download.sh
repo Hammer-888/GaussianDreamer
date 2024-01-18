f336a625c160f2f55d11e9254bc0b8f717a6aa4d
Biel-Hammer <creepui@gmail.com>
2024-01-18 17:15:01 +0800

    zero123

--- load/zero123/download.sh
+++ load/zero123/download.sh
@@ -1,1 +1,2 @@
-wget https://huggingface.co/cvlab/zero123-weights/resolve/main/105000.ckpt
+# wget https://huggingface.co/cvlab/zero123-weights/resolve/main/105000.ckpt
+wget https://huggingface.co/stabilityai/stable-zero123/blob/main/stable_zero123.ckpt
